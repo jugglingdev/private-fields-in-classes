5816d87128ab7cff608a9a486bbf3f078567043b
jugglingdev <kaylamarie1785@gmail.com>
2023-09-11 20:15:33 -0500

    Completed Exercise 6

--- person.js
+++ person.js
@@ -1,5 +1,14 @@
 class Person {
-  // Write code here
+  #id = Math.floor(Math.random() * 1000);
+
+  get getID() {
+    return this.#id;
+  }
+
+  set setID(id) {
+    return this.#id = id;
+  }
 }
+
 
 module.exports = Person;
